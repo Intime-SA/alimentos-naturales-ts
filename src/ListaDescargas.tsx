e3f79a7b91d5da7583f46af78c901672cffb8401
Intime-SA <invertimesa@gmail.com>
2024-09-21 15:14:55 -0300

    cambios trakeo final avatar

--- src/ListaDescargas.tsx
+++ src/ListaDescargas.tsx
@@ -54,16 +54,29 @@ const TrakeoAlimentosNaturales: React.FC = () => {
     return timestamp ? new Date(timestamp.seconds * 1000).toLocaleString() : "";
   };
 
+  // Función para generar un color aleatorio
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
   return (
     <ul role="list" className="divide-y divide-gray-100">
       {devices.map((device, index) => (
         <li key={index} className="flex justify-between gap-x-6 py-5">
           <div className="flex min-w-0 gap-x-4">
-            <img
-              className="h-12 w-12 flex-none rounded-full bg-gray-50"
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt={device.name}
-            />
+            <div
+              className="h-12 w-12 flex-none rounded-full flex items-center justify-center"
+              style={{ backgroundColor: getRandomColor() }}
+            >
+              <span className="text-white font-bold">
+                {device.email.charAt(0).toUpperCase()}
+              </span>
+            </div>
             <div className="min-w-0 flex-auto">
               <p className="text-sm font-semibold leading-6 text-gray-900">
                 {device.name}
