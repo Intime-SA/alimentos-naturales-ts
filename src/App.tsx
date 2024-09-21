11b38a0b969ddd0b7d1c35ae7270155da76ad888
Intime-SA <invertimesa@gmail.com>
2024-09-20 23:25:44 -0300

    cambios 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import Login from "./Login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-gray-100">
       <Login />
